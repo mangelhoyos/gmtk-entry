d44a7315a04819608a5223ef890437bc0556da48
mangelhoyos <mangelhoyos101@gmail.com>
2023-07-13 19:58:20 -0500

    Last visual settings

--- GMTK-Entry/Assets/Scripts/Core/GameManager.cs
+++ GMTK-Entry/Assets/Scripts/Core/GameManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    [SerializeField] private CanvasGroup fadeInGroup;
+
+    private bool gameOver = false;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Game over")]
+    public void GameOver()
     {
-        
+        if(!gameOver)
+        {
+            gameOver = true;
+            StartCoroutine(FadeScreenToBlack());
+        }
+    }
+
+    IEnumerator FadeScreenToBlack()
+    {
+        while(fadeInGroup.alpha < 1)
+        {
+            fadeInGroup.alpha += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("MainMenu");
     }
 }
